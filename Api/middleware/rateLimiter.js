7f5267764da14c8fc2629e1eb5ab2ce14cecb819
Hammad Raza <01hammadraza@gmail.com>
2026-05-12 13:56:47 +0500

    madtesting

--- Api/middleware/rateLimiter.js
+++ Api/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const apiRateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 100, 
   message: {
     error: 'Too many requests from this IP, please try again after a minute'
   },
